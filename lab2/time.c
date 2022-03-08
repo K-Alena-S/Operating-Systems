2885285450fcdec66a0acf92fe3ebacc51027ab4
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-03-08 16:38:31 +0700

    Update time.c

--- lab2/time.c
+++ lab2/time.c
@@ -2,17 +2,46 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
+#define SUCCESS 0;
+#define ERROR -1;
+#define PUTENV_EXIT_ERROR 1;
+#define TIME_EXIT_ERROR 2;
+#define CTIME_EXIT_ERROR 3;
+#define LOCALTIME_EXIT_ERROR 4;
+
 extern char *tzname[];
 
 main() {
     time_t now;
     struct tm *sp;
+       
+    int result_putenv = putenv ("TZ=America/Los_Angeles");
+    if (result_putenv == ERROR){
+		perror("Error - putenv");
+		exit(PUTENV_EXIT_ERROR);
+	}
     
-    (void) time( &now );
+    int all_time = time(&now);
+    if (all_time == ERROR) {
+        perror("Error - time");
+        exit(TIME_EXIT_ERROR);
+    }
     
-    printf("%s", ctime( &now ) );
+    char* str_time;
+    str_time = ctime(&now);
+    if (Ctime == NULL){
+        perror("Error - ctime");
+        exit(CTIME_EXIT_ERROR);
+    }
+    printf("%s", str_time);
     
     sp = localtime(&now);
+    if (sp == NULL){
+        perror("Error - localtime");
+        exit(LOCALTIME_EXIT_ERROR);
+    }
+    
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, 
         sp->tm_mday,
@@ -20,5 +49,6 @@ main() {
         sp->tm_hour,
         sp->tm_min, 
         tzname[sp->tm_isdst]);
-    exit(0);
+    
+    return SUCCESS;
 }
